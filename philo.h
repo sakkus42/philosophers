f37a65e53b5b6265aab0f198440515a3c34c8d69
sakkus42 <sakkus@k1m42s02.42istanbul.com.tr>
2023-03-04 18:52:22 +0300

    cumartesi_end

--- philo.h
+++ philo.h
@@ -5,21 +5,27 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <sys/time.h>
 
 typedef struct s_arg
 {
-	int	nbr_of_philo;
-	int	time_die;
-	int	time_eat;
-	int	time_sleep;
-	int	total_eat;
+	int	nbr_of_philo; // philo sayısı
+	int	time_die; // philo yemeye başlamamışsa ölme süresi
+	int	time_eat; //philo yemek yeme süresi
+	int	time_sleep; //philo uyuyarak geçireceği zaman
+	int	total_eat; // optional argument simulasyon süresi
 }	t_arg;
 
 typedef struct s_philo
 {
+	int				fork;
+	int				m_second;
 	int				phil_ind;
-	pthread_t		philo;
+	t_arg			*arg;
+	pthread_t		th_philo;
 	pthread_mutex_t	mutex_Philo;
+	pthread_mutex_t	mutex_fork_r;
+	pthread_mutex_t	mutex_fork_l;
 }	t_philo;
 
 
